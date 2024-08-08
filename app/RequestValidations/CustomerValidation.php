50ea3b679b9a08a00e6623173f6ed7189a0d5b93
khanh228ey <khanh2282002dzz@gmail.com>
2024-08-08 16:40:53 +0700

    fix validate customer

--- app/RequestValidations/CustomerValidation.php
+++ app/RequestValidations/CustomerValidation.php
@@ -7,7 +7,6 @@ class CustomerValidation{
     public function customerValidation()
     {
         $rules = [
-            'email' => 'unique:customers,email,'.request()->id.'|max:100',
             'name' => 'required|max:100',
             'phone_number' => 'required|unique:customers,phone_number,'.request()->id.'|numeric',
             'birthday' => 'required',
@@ -15,11 +14,6 @@ class CustomerValidation{
         $messages = [
             'name.required' => 'Tên không được bỏ trống',
             'name.max' => 'Tên không được quá 100 kí tự',
-            'email.max' => 'Email đã vượt quá 100 kí tự',
-            'email.unique' => 'Email đã được sử dụng',
-            'phone_number.unique' => 'Số điện thoại đã được sử dụng',
-            'phone_number.required' => 'Số điện thoại không được rỗng',
-            'phone_number.numeric' => 'Số điện thoại phải là số',
             'birthday.required' => 'Ngày sinh không được để trống',
         ];
         $validator = Validator::make(
